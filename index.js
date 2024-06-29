89ed12cb75aab76dc41f1dc57aef2e863b69f588
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2024-06-29 14:01:59 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+{
+    let num1 = 31;
+    let num2 = 2;
+    let multiply = num1 * num2;
+    console.log(multiply);
+}
+
+{
+    let random = Math.floor(Math.random() * 10) + 1;
+}
+
+{
+    let num3 = 10;
+    let num4 = 6;
+    let mod = num3 % num4;
+    console.log(mod);
+}
+
+{
+    let max = Math.max(0, 20);
+}
